8111d039b0f34e7425516040dfdad4f9685bc12c
Maxime Fagnan <fagnan.maxime@courrier.uqam.ca>
2023-12-04 14:40:34 -0500

    accepter les input avec doublons dans j3

--- Advent of code/2023/j3.py
+++ Advent of code/2023/j3.py
@@ -40,8 +40,8 @@ print(soln)
 # Part 2:
 
 soln = 0
-numbers = dict()
-stars = dict()
+numbers = dict() #(row,col) where there is a digit -> number
+stars = dict() #(row,col) where there is a * -> 1
 
 for row,line in zip(range(rows)[1:rows-1],lines[1:columns-1]):
     matches = re.finditer("[0-9]+",line)
@@ -57,16 +57,34 @@ for row,line in zip(range(rows)[1:rows-1],lines[1:columns-1]):
         stars[(row,m.start())] = 1
 
 for row,col in stars:
+
+    #skip this star if it doesn't have exactly two adjacent numbers
+    up = "".join(char_matrix[row-1][col-1:col+2])
+    left = char_matrix[row][col-1]
+    right = char_matrix[row][col+1]
+    down = "".join(char_matrix[row+1][col-1:col+2])
+    neighbourhood = "_".join([up,left,right,down])
+    # .82..
+    # ..*..  -> 82._._._.82   (for linear search)
+    # ..82.
+    if len(re.findall("[0-9]+", neighbourhood)) !=2 : continue
+
+    #find the two adjacent numbers
     adjacent_numbers = set()
+    # set is necessary since 82 would be picked up 4 times in the following example:
+    # .82..
+    # ..*..
+    # ..82.
     for i in range(row-1,row+2):
         for j in range(col-1, col+2):
             if i==row and j==col: continue # This is the star itself
             number = numbers.get((i,j)) # None if there is no number at those coords.
             if number :
                 adjacent_numbers.add(number)
-                #note that this wouldn't work if there were duplicates of a number around the same star
 
     if len(adjacent_numbers) == 2:
         soln += adjacent_numbers.pop() * adjacent_numbers.pop()
+    elif len(adjacent_numbers) == 1: #if the two adjacent numbers were the same number. 
+        soln += adjacent_numbers.pop()**2
 
 print(soln)
